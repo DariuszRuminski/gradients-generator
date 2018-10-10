bc9a71a2f3bd4fefcdc42de476887952d62e22e5
DariuszRuminski <darekr1992@interia.pl>
2018-10-10 02:49:52 +0200

    fff

--- main.js
+++ main.js
@@ -1,29 +1,53 @@
 const btn = document.querySelector('.btn');
-const box = document.querySelector('.box');
-const info = document.querySelector('.info');
-const info2 = document.querySelector('.info_2');
-const copy = document.querySelector('.copy');
+const cnt1 = document.querySelector('.content_1');
+const cnt2 = document.querySelector('.content_2');
+const col1 = document.querySelector('.col_1');
+const col1Info = document.querySelector('.col_1-info');
+const col2 = document.querySelector('.col_2');
+const col2Info = document.querySelector('.col_2-info');
 
-btn.addEventListener('click', function(){ 
-    let rgb_1;
-    let rgb_2;
+const tab = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+let color_1 = '#';
+let color_2 = '#';
+let counter = 0;
+
+btn.addEventListener('click', function(){
+    for(let i=0;i<6;i++){
+        const zmienna_1 = Math.floor(Math.random()*16);
+        const zmienna_2 = Math.floor(Math.random()*16);
+        color_1 += tab[zmienna_1];
+        color_2 += tab[zmienna_2];
+        col1.style.setProperty('background-color',color_1);
+        col2.style.setProperty('background-color',color_2);
+        col1Info.innerText = color_1;
+        col2Info.innerText = color_2;
+    }
+    const kat = Math.floor(Math.random()*361);
     
-        let color_1 = Math.floor(Math.random() * (255+1));
-        let color_2 = Math.floor(Math.random() * (255+1));
-        let color_3 = Math.floor(Math.random() * (255+1));
-        rgb_1 = 'rgb('+color_1+','+color_2+','+color_3+')';
+    if(counter%2==0){
+        cnt2.style.setProperty('background-image', 'linear-gradient('+kat+'deg,'+ color_1+','+color_2+')');
+    }
+    else{
+        cnt1.style.setProperty('background-image', 'linear-gradient('+kat+'deg,'+ color_1+','+color_2+')');
+    }
     
-        let color_4 = Math.floor(Math.random() * (255+1));
-        let color_5 = Math.floor(Math.random() * (255+1));
-        let color_6 = Math.floor(Math.random() * (255+1));
-        rgb_2 = 'rgb('+color_4+','+color_5+','+color_6+')';
+    cnt1.classList.toggle('b');
+    cnt2.classList.toggle('a');
     
-    let text = 'linear-gradient(to right, ' + rgb_1 + ', ' + rgb_2+')';
-    info.innerText=rgb_1;
-    info2.innerText=rgb_2;
-    copy.innerText= 'background-image: linear-gradient(to right, '+ rgb_1 + ', ' + rgb_2+');';
+    console.log('kolor 1: ' + color_1);
+    console.log('kolor 2: ' + color_2);
+    console.log('kat: ' + kat);
     
-    box.style.setProperty('background-image',text);
-    box.style.setProperty('transition','background-image 1s');
-    
+    color_1 = '#';
+    color_2 = '#';
+    counter++;    
 });
+
+btn.addEventListener('mousedown', function(){
+    btn.classList.add('btn-click');
+});
+btn.addEventListener('mouseup', function(){
+    btn.classList.remove('btn-click');
+});
+
+
