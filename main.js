4b2aebbba6cd006e8a6f6abbcc6a271878db6e24
DariuszRuminski <darekr1992@interia.pl>
2018-11-15 18:19:38 +0100

    rotate - mobile test

--- main.js
+++ main.js
@@ -21,40 +21,6 @@ menuBtn.addEventListener('click', function(){
     });
 });
 
-const r=document.querySelector('.r');
-const g=document.querySelector('.g');
-const b=document.querySelector('.b');
-const r2=document.querySelector('.r2');
-const g2=document.querySelector('.g2');
-const b2=document.querySelector('.b2');
-
-
-const randomBtn = document.querySelector('.random');
-const gradient = document.querySelector('.gradient-box');
-const randomColor = [];
-let randomColorOne = '';
-let randomColorTwo = '';
-let randomRotate = '';
-randomBtn.addEventListener('click', function(){
-    for(let i=0; i<6 ;i++){
-        randomColor[i] = Math.floor(Math.random()*256);
-    }
-    randomColorOne = 'rgb('+randomColor[0]+','+randomColor[1]+','+randomColor[2]+')';
-    randomColorTwo = 'rgb('+randomColor[3]+','+randomColor[4]+','+randomColor[5]+')';
-    randomRotate = Math.floor(Math.random()*361) + 'deg';
-            r.value = randomColor[0];
-            g.value = randomColor[1];
-            b.value = randomColor[2];
-            r2.value = randomColor[3];
-            g2.value = randomColor[4];
-            b2.value = randomColor[5];
-    
-    gradient.style.setProperty('background-image', 'linear-gradient('+randomRotate+','+randomColorOne+','+randomColorTwo+')');
-});
-
-
-
-
 const colorsBtn = document.querySelectorAll('.colors-box-btn button');
 const colorsBoxInput = document.querySelectorAll('.colors-box-input');
 const colorsInput = document.querySelectorAll('.colors-box-input input');
@@ -77,13 +43,29 @@ for(let color of colorsBtn){
 };
 
 const inputChange = function(){
-    let x ='linear-gradient('+randomRotate+',rgb('+r.value+','+g.value+','+b.value+')'+',rgb('+r2.value+','+g2.value+','+b2.value+'))';
-    gradient.style.setProperty('background-image', x);
+    gradient.style.setProperty('background-image', 'linear-gradient('+randomRotate+',rgb('+colorsInput[0].value+','+colorsInput[1].value+','+colorsInput[2].value+')'+',rgb('+colorsInput[3].value+','+colorsInput[4].value+','+colorsInput[5].value+'))');
 }
 for(let el of colorsInput){
     el.addEventListener('input', inputChange);
 }
 
+const randomBtn = document.querySelector('.random');
+const gradient = document.querySelector('.gradient-box');
+const randomColor = [];
+let randomColorOne = '';
+let randomColorTwo = '';
+let randomRotate = '';
+randomBtn.addEventListener('click', function(){
+    for(let i=0; i<6 ;i++){
+        randomColor[i] = Math.floor(Math.random()*256);
+        colorsInput[i].value = randomColor[i];
+    }
+    randomColorOne = 'rgb('+randomColor[0]+','+randomColor[1]+','+randomColor[2]+')';
+    randomColorTwo = 'rgb('+randomColor[3]+','+randomColor[4]+','+randomColor[5]+')';
+    randomRotate = Math.floor(Math.random()*361) + 'deg';
+    
+    gradient.style.setProperty('background-image', 'linear-gradient('+randomRotate+','+randomColorOne+','+randomColorTwo+')');
+});
 
 const orientationChange = function(){
     for(el of links){
@@ -93,3 +75,44 @@ const orientationChange = function(){
 window.addEventListener('resize', orientationChange);
 window.addEventListener('load', orientationChange);
 
+
+const rotate = document.querySelector('.rotate-box');
+const rotateCnt = document.querySelector('.rotate-content');
+const rotatePointer = document.querySelector('.rotate-pointer');
+const gradientHeight = document.querySelector('.gradient-box');
+
+const rotateAngle = document.querySelector('.rotate-angle');
+
+rotate.addEventListener('mousedown', function(e){
+    const x = (e.clientX - this.offsetLeft);
+    const xx = rotate.clientWidth / 2 + 1;
+    const xxx = x - xx;
+    
+    const y = e.clientY - this.offsetTop - gradientHeight.clientHeight;
+    const yy = y * (-1) + 55;
+    
+    const result = Math.atan2(xxx, yy)* 180 / Math.PI;
+    console.log(result + '*');
+    rotateAngle.innerText = Math.ceil(result)+'*';
+    
+    console.log(xxx);
+    console.log(yy);
+    rotateCnt.style.setProperty('transform','rotate('+result+'deg)');
+});
+
+rotate.addEventListener('touchstart', function(e){
+    const x = (e.clientX - this.offsetLeft);
+    const xx = rotate.clientWidth / 2 + 1;
+    const xxx = x - xx;
+    
+    const y = e.clientY - this.offsetTop - gradientHeight.clientHeight;
+    const yy = y * (-1) + 55;
+    
+    const result = Math.atan2(xxx, yy)* 180 / Math.PI;
+    console.log(result + '*');
+    rotateAngle.innerText = Math.ceil(result)+'*';
+    
+    console.log(xxx);
+    console.log(yy);
+    rotateCnt.style.setProperty('transform','rotate('+result+'deg)');
+});
